d73846e93c0e896504e9ed126d8100de1126004b
jeffspatafora <jeffspatafora@gmail.com>
2022-05-02 22:35:44 -0700

    adds initial index page with index action and button, need to add user signup, login

---
+++ src/views/UserNotesIndex.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      message: "All notes",
+      usernotes: []
+    };
+  },
+  created: function () { },
+  methods: {
+    indexUserNotes: function () {
+      // console.log("in user notes");
+      axios.get("/user_notes.json").then(response => {
+        console.log(response, response.data);
+      })
+    }
+  },
+};
+</script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+  </div>
+  <button v-on:click="indexUserNotes()">all user notes</button>
+</template>
+
+<style>
+</style>
