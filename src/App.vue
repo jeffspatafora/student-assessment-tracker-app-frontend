02e1f24f6c035de60148a8621fe4e7dca43b999b
jeffspatafora <jeffspatafora@gmail.com>
2022-05-06 13:15:31 -0700

    adds boilerplate code to HomeView.vue and comments out AboutView router link

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
+    <!-- <router-link to="/about">About</router-link> | -->
     <router-link to="/usernotesindex">notes</router-link> |
     <router-link to="/usernotescreate">create a note</router-link> |
     <router-link to="/signup">Signup</router-link> |
