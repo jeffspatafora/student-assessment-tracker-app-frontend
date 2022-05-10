65c257d7417732b94b6f23a96e3a3ef4dca0bcc3
jeffspatafora <jeffspatafora@gmail.com>
2022-05-10 11:29:36 -0700

    working on adding drop down for selecting notes index view for one student and one project

--- src/views/UserNotesIndex.vue
+++ src/views/UserNotesIndex.vue
@@ -5,20 +5,38 @@ export default {
   data: function () {
     return {
       message: "All notes",
-      userNotes: []
+      userNotes: [],
+      students: [],
+      projects: []
     };
   },
   created: function () {
     this.indexUserNotes();
+    this.indexProjects();
+    this.indexStudents();
   },
   methods: {
     indexUserNotes: function () {
-      // console.log("in user notes");
+      console.log("in user notes");
       axios.get("/user_notes.json").then(response => {
         console.log(response, response.data);
         this.userNotes = response.data;
       });
     },
+    indexProjects: function () {
+      console.log("in projects");
+      axios.get("/projects").then(response => {
+        console.log(response.data);
+        this.projects = response.data;
+      })
+    },
+    indexStudents: function () {
+      console.log("in students");
+      axios.get("/students").then(response => {
+        console.log(response.data);
+        this.students = response.data;
+      })
+    }
   },
 };
 </script>
@@ -27,6 +45,20 @@ export default {
   <div class="home">
     <h1>{{ message }}</h1>
   </div>
+
+  <p>
+    <label>Choose a project: </label>
+    <select v-model="projects.title">
+      <option v-for="project in projects" v-bind:key="project.id">{{ project.title }}</option>
+    </select>
+  </p>
+  <p>
+    <label>Choose a student: </label>
+    <select v-model="students.name">
+      <option v-for="student in students" v-bind:key="student.id">{{ student.name }}</option>
+    </select>
+  </p>
+
   <div v-for="note in userNotes" v-bind:key="note.id">
     <h4>{{ note.student }}</h4>
     <h5>{{ note.user }}</h5>
@@ -36,7 +68,6 @@ export default {
     <hr />
   </div>
 
-  <!-- <button v-on:click="indexUserNotes()">all user notes</button> -->
 </template>
 
 <style>
