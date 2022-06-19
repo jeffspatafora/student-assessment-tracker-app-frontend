d295d2f72b1ea6d5240deac94567f894a3364798
jeffspatafora <jeffspatafora@gmail.com>
2022-06-18 19:57:56 -0700

    modifies StudentCreate.vue to work with student image upload, uses AboutView.vue for testing

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,51 @@
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      message: "Add a Student",
+      image: "",
+      studentName: "",
+      studentEmail: ""
+    };
+  },
+  created: function () { },
+  methods: {
+    setFile: function (event) {
+      if (event.target.files.length > 0) {
+        this.image = event.target.files[0];
+      }
+    },
+    submit: function () {
+      var formData = new FormData();
+      formData.append("name", this.studentName);
+      formData.append("email", this.studentEmail);
+      formData.append("image", this.image);
+
+      axios.post("/students", formData).then(response => {
+        console.log(response.data);
+        this.$refs.fileInput.value = "";
+        this.$router.push('/students');
+      });
+    },
+  },
+};
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+
+  <div>
+    <form v-on:submit.prevent="submit()">
+      <p>Name: <input type="text" v-model="studentName" /></p>
+      <p>Email: <input type="text" v-model="studentEmail" /></p>
+      <p>
+        Image: <input type="file" v-on:change="setFile($event)" ref="fileInput">
+      </p>
+      <input type="submit" value="Create Student">
+    </form>
   </div>
 </template>
+
+<style>
+</style>
--- src/views/StudentCreate.vue
+++ src/views/StudentCreate.vue
@@ -5,6 +5,62 @@ export default {
   data: function () {
     return {
       message: "add a student",
+      image: "",
+      studentName: "",
+      studentEmail: ""
+    };
+  },
+  created: function () { },
+  methods: {
+    setFile: function (event) {
+      if (event.target.files.length > 0) {
+        this.image = event.target.files[0];
+      }
+    },
+    submit: function () {
+      var formData = new FormData();
+      formData.append("name", this.studentName);
+      formData.append("email", this.studentEmail);
+      formData.append("image", this.image);
+
+      axios.post("/students", formData).then(response => {
+        console.log(response.data);
+        this.$refs.fileInput.value = "";
+        this.$router.push('/students');
+      });
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="students-create">
+    <h1>{{ message }}</h1>
+  </div>
+  <div>
+    <form v-on:submit.prevent="submit()">
+      <p>Name: <input type="text" v-model="studentName" /></p>
+      <p>Email: <input type="text" v-model="studentEmail" /></p>
+      <p>
+        Image: <input type="file" v-on:change="setFile($event)" ref="fileInput">
+      </p>
+      <input type="submit" value="Create Student">
+    </form>
+  </div>
+</template>
+
+<style>
+</style>
+
+
+
+<!-- <script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      message: "add a student",
       students: [],
       newStudentParams: {}
     };
@@ -35,4 +91,4 @@ export default {
 </template>
 
 <style>
-</style>
+</style> -->
